2f1db9a7a90df81bc3ed0147f8cbebdf9dc3f9be
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2026-03-07 13:09:02 -0300

    Ajustando posição do componente addButton

--- src/components/addButton/styleAddButton.tsx
+++ src/components/addButton/styleAddButton.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components/native';
 
 export const Button = styled.TouchableOpacity`
   position: absolute;
-  bottom: 24px;
-  right: 24px;
+  bottom: 60px;
+  right: 16px;
   width: 56px;
   height: 56px;
   border-radius: 28px;
@@ -15,5 +15,5 @@ export const Button = styled.TouchableOpacity`
   shadow-offset: 0px 4px;
   shadow-opacity: 0.3;
   shadow-radius: 4px;
-  elevation: 8;
+  elevation: 6;
 `;
