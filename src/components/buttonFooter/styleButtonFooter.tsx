7dad0be6a2df78f5d4787ca498ed5df1a77a2fa8
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2026-03-07 13:11:35 -0300

    Ajustando componente buttonFooter

--- src/components/buttonFooter/styleButtonFooter.tsx
+++ src/components/buttonFooter/styleButtonFooter.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex-direction: row;
   padding: 20px;
-  padding-bottom: 30px;
+  padding-bottom: 50px;
   gap: 12px;
   background-color: #fff;
   border-top-width: 1px;
